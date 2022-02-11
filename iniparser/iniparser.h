d19604434ed392875bc94b0120ee1a5dee2ab0d2
Krisna Pranav <krisna.pranav@gmail.com>
2022-02-11 19:02:03 +0530

    iniparser: iniparser header

--- iniparser/iniparser.h
+++ iniparser/iniparser.h
@@ -11,6 +11,54 @@
 
 #pragma once
 
+#include <map>
+#include <assert.h>
+#include <iostream>
+
+/**
+ * @brief contains the classes and functions 
+ * 
+ */
 namespace iniparser {
-    // TODO...
+
+    /**
+     * @brief whie spaces
+     * 
+     * @return constexpr const char* 
+     */
+    constexpr const char *whitespaces() {
+        return " \t\n\r\f\v";
+    }
+
+    /**
+     * @brief trim function 
+     * 
+     * @param str 
+     */
+    inline void trim(std::string &str) {
+        auto lastpos = str.find_last_not_of(whitespaces());
+        if (lastpos == std::string::npos) {
+            str.clear();
+            return;
+        }
+
+        str.erase(lastpos + 1);
+        str.erase(0, str.find_first_of(whitespaces()));
+    }
+
+    /**
+     * @brief trim copy function
+     * 
+     * @param str 
+     * @return std::string 
+     */
+    inline std::string trimCopy(const std::string &str) {
+        auto firstpos = str.find_first_not_of(whitespaces());
+        if (firstpos == std::string::npos)
+            return "";
+        
+        auto lastpos = str.find_last_not_of(whitespcaes());
+        return str.substr(firstpos, lastpos);
+    } 
+
 }
