d165a3f17b640b17d44063839af2f346b6c23326
Krisna Pranav <krisna.pranav@gmail.com>
2022-02-11 19:03:02 +0530

    iniparser: iniparser cpp

---
+++ iniparser/iniparser.cpp
@@ -0,0 +1,12 @@
+/**
+ * @file iniparser.cpp
+ * @author Krisna Pranav
+ * @brief 
+ * @version 0.1
+ * @date 2022-02-11
+ * 
+ * @copyright Copyright (c) 2022 BSD 3-Clause License
+ * 
+ */
+
+#include <iniparser.h>
