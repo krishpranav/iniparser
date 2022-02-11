8f12581edd4bda1fc1beef0e3e3e247b2a28250d
Krisna Pranav <krisna.pranav@gmail.com>
2022-02-11 18:51:03 +0530

    add files via upload
    add: CMakeLists + .gitignore + iniparser header file

---
+++ iniparser/iniparser.h
@@ -0,0 +1,16 @@
+/**
+ * @file iniparser.h
+ * @author Krisna Pranav
+ * @brief Ini Parser Main Header File 
+ * @version 0.1
+ * @date 2022-02-11
+ * 
+ * @copyright Copyright (c) 2022 MIT License
+ * 
+ */
+
+#pragma once
+
+namespace iniparser {
+    // TODO...
+}
